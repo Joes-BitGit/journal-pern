22ceb6359649ee05ebeb2fb5a64e827f6b2207d2
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-27 16:15:24 -0800

    pg  SQL commands for API

--- server/db.sql
+++ server/db.sql
@@ -13,3 +13,9 @@ CREATE TABLE entries (
 
 INSERT INTO entries(time, location,how,personal,professional,feel) 
 VALUES ('January 8,1999', 'Los Angeles', 2, 'personally grateful for...', 'professionally grateful for...', 'what''s on your mind today?');
+
+SELECT * FROM entries;
+
+UPDATE entries SET time=$1, location=$2, how=$3, personal=$4, professional=$5, feel=$6 WHERE id=$7 returning *;
+
+DELETE FROM entries WHERE id=$1;
