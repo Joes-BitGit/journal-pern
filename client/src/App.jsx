3d084d0bbc3721cba9bedb3466a0c892c8bc2f7d
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-29 15:51:44 -0800

    React Router init 3 routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Entries from './routes/Entries.jsx'
+import UpdatePage from './routes/UpdatePage.jsx'
 
 const App = () => {
-  return <div className="">REACT APP</div>
+  return (
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}/>  
+          <Route exact path='/entries' component={Entries}/>  
+          <Route exact path='/entries/:id/update' component={UpdatePage}/>  
+        </Switch>  
+      </Router>  
+    </div>
+  );
 }
 
 export default App;
