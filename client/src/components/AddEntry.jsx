a4bb514e057b304f981af906b0f136849922a044
Joes-BitGit <josephnalmeida@gmail.com>
2021-02-06 16:46:22 -0800

    Form validation + formatting

--- client/src/components/AddEntry.jsx
+++ client/src/components/AddEntry.jsx
@@ -50,17 +50,38 @@ const AddEntry = () => {
         <div className="form-row mb-3">
           {/* Location */}
           <div className="col">
-            <input value={where} onChange={e => setWhere(e.target.value)} type="text" name="location" className="form-control" placeholder="Location" />
+            <input
+              value={where}
+              onChange={e => setWhere(e.target.value)}
+              type="text"
+              name="location"
+              className="form-control"
+              placeholder="Location"
+              required
+            />
           </div>
           {/* Date */}
           <div className="col">
-            <input value={when} onChange={e => setWhen(e.target.value)} type="date" name="date" className="form-control" />
+            <input
+              value={when}
+              onChange={e => setWhen(e.target.value)}
+              type="date"
+              name="date"
+              className="form-control"
+              required
+            />
           </div>
         </div>
         <div className="form-group">
           {/* Feeling Level */}
-          <label htmlFor="feeling">Feeling Level</label>
-          <select name="feeling" className="custom-select" value={feel} onChange={e => setFeel(e.target.value)} >
+          <label htmlFor="Feeling">Feeling Level</label>
+          <select
+            name="feeling"
+            className="custom-select"
+            value={feel}
+            onChange={e => setFeel(e.target.value)}
+            required
+          >
             <option disabled >Feeling</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -72,37 +93,77 @@ const AddEntry = () => {
         <div className="form-group">
           {/* Personal */}
           <label htmlFor="personally-grateful">Personally Grateful</label>
-          <textarea name="personally-grateful" cols="30" rows="5" className="form-control" placeholder="I am personally grateful for..." value={personal} onChange={e => setPersonal(e.target.value)}></textarea>
+          <textarea
+            name="personally-grateful"
+            cols="30"
+            rows="5"
+            className="form-control"
+            placeholder="I am personally grateful for..."
+            value={personal}
+            onChange={e => setPersonal(e.target.value)}
+            required
+          ></textarea>
         </div>
         <div className="form-group">
           {/* Professional */}
           <label htmlFor="professionally-grateful">Professionally Grateful</label>
-          <textarea name="professionally-grateful" cols="30" rows="5" className="form-control" placeholder="I am professionally grateful for..."
-            value={professional} onChange={e => setProfessional(e.target.value)}></textarea>
+          <textarea
+            name="professionally-grateful"
+            cols="30"
+            rows="5"
+            className="form-control"
+            placeholder="I am professionally grateful for..."
+            value={professional}
+            onChange={e => setProfessional(e.target.value)}
+            required
+          ></textarea>
         </div>
         <div className="form-group">
           {/* Mind */}
           <label htmlFor="mindfullness">Mindfullness</label>
-          <textarea name="mindfullness" cols="30" rows="5" className="form-control" placeholder="What's on your mind today?" value={mindful}
-            onChange={e => setMindful(e.target.value)}></textarea>
+          <textarea
+            name="mindfullness"
+            cols="30" rows="5"
+            className="form-control"
+            placeholder="What's on your mind today?"
+            value={mindful}
+            onChange={e => setMindful(e.target.value)}
+            required
+          ></textarea>
         </div>
         <label htmlFor="Goal">Goal</label>
         <div className="input-group mb-3">
           {/* Goal */}
           <div className="input-group-prepend">
             <div className="input-group-text">
-              <input type="checkbox" checked={goal} onChange={handleCheck} />
+              <input
+                type="checkbox"
+                checked={goal}
+                onChange={handleCheck}
+                required
+              />
             </div>
           </div>
-          <input type="text" className="form-control" placeholder="Goal of the Day" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Goal of the Day"
+            required
+          />
         </div>
         {/* Buttons */}
         <div className="form-row">
           <div className="col">
-            <button type="submit" className="btn btn-outline-dark" onClick={handleView}>View All Entries</button>
+            <button
+              type="submit"
+              className="btn btn-outline-dark"
+              onClick={handleView}>View All Entries</button>
           </div>
           <div className="col-auto pb-1">
-            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Add</button>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}>Add</button>
           </div>
         </div>
       </form>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1 className="text-center display-2">JOURNAL</h1>
+      <p className="text-center">(Must Enter All Fields)</p>
     </div>
   )
 }
