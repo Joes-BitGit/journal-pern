5be9de83a65aeaa52bd5da7318209cbfe72a62ec
Joes-BitGit <josephnalmeida@gmail.com>
2021-02-06 16:46:00 -0800

    Init detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './routes/Home.jsx'
 import Entries from './routes/Entries.jsx'
 import UpdatePage from './routes/UpdatePage.jsx'
+import DetailEntry from './routes/DetailEntry.jsx'
 import { EntriesContextProvider } from './context/EntriesContext.js';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
             <Route exact path='/' component={Home} />
             <Route exact path='/entries' component={Entries} />
             <Route exact path='/entries/:id/update' component={UpdatePage} />
+            <Route exact path='/entries/:id' component={DetailEntry} />
           </Switch>
         </Router>
       </div>
---
+++ client/src/routes/DetailEntry.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DetailEntry = () => {
+  return (
+    <h1 className='text-center'>
+      👨‍💻
+    </h1>
+  )
+}
+
+export default DetailEntry
